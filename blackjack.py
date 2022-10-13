92a57e686b65d4855e9c301ea525aded65dbb72b
Corey <coreydblevins@gmail.com>
2022-10-13 11:59:22 -0400

    added rounds

--- blackjack.py
+++ blackjack.py
@@ -7,76 +7,145 @@ RANK_VALUE = {'A': 11, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7,
 class Game:
     def __init__(self):
         self.deck = Deck()
-        self.deck.shuffle_deck()
         self.player = Player()
         self.dealer = Dealer()
-        self.deal_card(self.player)
-        self.deal_card(self.player)
-        self.deal_card(self.dealer)
-        self.deal_card(self.dealer)
+        self.tally = 1
+        self.start_game()
 
+    def start_game(self):
+        self.deck.cards.clear()
+        self.deck.build_deck()
+        self.player.hand.clear()
+        self.dealer.hand.clear()
+        self.deck.shuffle_deck()
+        self.deal_card(self.player)
+        self.deal_card(self.player)
+        self.deal_card(self.dealer)
+        self.deal_card(self.dealer)
+        print(f"Round: {self.tally}")
+        print(f"Deck is {len(self.deck.cards)}")
+        print("\nDealer's cards: ")
+        print(f"{self.dealer.hand[0]}")
+        print("???")
         print("\nPlayer's cards: ")
         for card in self.player.hand:
             print(card)
         print(f"This hand is worth: {self.calculate_hand(self.player)}")
-        print("\nDealer's cards: ")
-        print(f"{self.dealer.hand[0]}")
-        print("???")
         self.player_acts()
-        
 
     def deal_card(self, person):
         card = self.deck.cards.pop()
         person.hand.append(card)
 
     def calculate_hand(self, person):
-        total_points = 0
+        total_points = 0        
         for card in person.hand:
             total_points += card.value
         return total_points
     
     def player_acts(self):
-        if self.calculate_hand(self.player) == 21:
-            print("Blackjack! You win!")
+        self.player_twenty_one = False
+        if len(self.player.hand) == 2 and self.calculate_hand(self.player) == 21:
+            print("\n *Blackjack! You win!*")
+            self.continue_playing()
+        elif len(self.player.hand) > 2 and self.calculate_hand(self.player) == 21:
+            self.player_twenty_one = True
+            print("You have 21!")
+            self.dealer_acts()
+            self.end_round_score()
+            self.continue_playing()
         elif self.calculate_hand(self.player) < 21:
-            print("\nWould you like to hit or stay?")
-            response = input("Enter (h) for hit or (s) for stay: ")
-            if response == "h":
-                self.deal_card(self.player)
-                for card in self.player.hand:
-                    print(card)
-                print(f"This hand is now worth: {self.calculate_hand(self.player)}")
-                self.player_acts()
-            if response == "s":
-                self.dealer_acts()
+            self.hit_or_stay()
         elif self.calculate_hand(self.player) > 21:
             print("Bust!")
+            self.continue_playing()
+
+    def hit_or_stay(self):
+        print("\nWould you like to hit or stay?")
+        response = input("Enter (h) for hit or (s) for stay: ").lower()
+        if response == "h":
+            self.deal_card(self.player)
+            for card in self.player.hand:
+                print(card)
+            print(f"Your hand is now worth: {self.calculate_hand(self.player)}")
+            self.player_acts()
+        elif response == "s":
+            self.dealer_acts()
+        else:
+            self.hit_or_stay()
 
     def dealer_acts(self):
         print(" ")
-        if self.calculate_hand(self.dealer) == 21:
+        self.dealer_blackjack = False
+        self.dealer_twenty_one = False
+        self.dealer_bust = False
+        if len(self.dealer.hand) == 2 and self.calculate_hand(self.dealer) == 21:
+            self.dealer_blackjack = True
             for card in self.dealer.hand:
                 print(card)
             print(f"Dealer's hand is worth: {self.calculate_hand(self.dealer)}")
             print("Dealer has blackjack!")
+            self.end_round_score()
+            self.continue_playing()
+        elif len(self.dealer.hand) > 2 and self.calculate_hand(self.dealer) == 21:
+            self.dealer_twenty_one = True
+            for card in self.dealer.hand:
+                print(card)
+            print(f"Dealer's hand is worth: {self.calculate_hand(self.dealer)}")
+            print("Dealer has 21!")
+            self.end_round_score()
+            self.continue_playing()
         elif self.calculate_hand(self.dealer) < 17:
             for card in self.dealer.hand:
                 print(card)
             print(f"Dealer's hand is worth: {self.calculate_hand(self.dealer)}")
             self.deal_card(self.dealer)
-            print(f"Dealer hits! {self.dealer.hand[-1]}")      
+            print(f"Dealer hits! {self.dealer.hand[-1]}")
             self.dealer_acts()
         elif self.calculate_hand(self.dealer) > 21:
+            self.dealer_bust = True
             for card in self.dealer.hand:
                 print(card)
             print(f"Dealer's hand is worth: {self.calculate_hand(self.dealer)}")
             print("Dealer bust!")
+            self.end_round_score()
+            self.continue_playing()
         else:
             for card in self.dealer.hand:
                 print(card)
             print(f"Dealer's hand is worth: {self.calculate_hand(self.dealer)}")
             print(f"Dealer stays with a hand worth: {self.calculate_hand(self.dealer)}")
+            self.end_round_score()
+            self.continue_playing()
 
+    def end_round_score(self):
+        print(f"\n  Player hand value: {self.calculate_hand(self.player)}")
+        print(f"  Dealer hand value: {self.calculate_hand(self.dealer)}")
+        if self.dealer_blackjack is True:
+            print("    *Dealer has blackjack!*")
+        elif self.dealer_twenty_one is True and self.player_twenty_one is False:
+            print("    Dealer has 21!")
+        elif self.dealer_twenty_one is True and self.player_twenty_one is True:
+            print("    Push")
+        elif self.dealer_bust is True:
+            print("    Dealer bust!")
+        elif self.calculate_hand(self.player) > self.calculate_hand(self.dealer):
+            print("    *Player wins!*")
+        elif self.calculate_hand(self.player) == self.calculate_hand(self.dealer):
+            print("    Push")
+        else:
+            print("    Dealer wins.")         
+
+    def continue_playing(self):
+        print("\nWould you like to keep playing?")
+        play_response = input("Enter (y) for yes or (n) for no: ")
+        if play_response == "y":
+            self.tally += 1
+            self.start_game()
+        elif play_response == "n":
+            exit()
+        else:
+            self.continue_playing()
 
 class Card:
     def __init__(self, suit, rank, value):
@@ -89,6 +158,7 @@ class Card:
 class Deck:
     def __init__(self):
         self.cards = []
+    def build_deck(self):
         for suit in SUITS:
             for rank_value in RANK_VALUE.items():
                 new_card = Card(suit, rank_value[0], rank_value[1])
@@ -96,17 +166,15 @@ class Deck:
     def shuffle_deck(self):
         random.shuffle(self.cards)
 
-        
+
 class Player:
     def __init__(self):
         self.hand = []
         pass
-    
+
 class Dealer:
     def __init__(self):
         self.hand = []
         pass
-    
-new_game = Game()
 
-    
+Game()
